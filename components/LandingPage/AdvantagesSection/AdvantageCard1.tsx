24dbcf72de8f8362706af07de5cd4d16f73c7892
Yousef <yousef.ashraf@syadtech.com>
2024-09-08 11:44:01 +0300

    Finished Advantages section

--- components/LandingPage/AdvantagesSection/AdvantageCard1.tsx
+++ components/LandingPage/AdvantagesSection/AdvantageCard1.tsx
@@ -1,23 +1,14 @@
 "use client";
 
 import { cn } from "@nextui-org/react";
+import { motion, Variants } from "framer-motion";
 import Image from "next/image";
 import AdvantageCard from "./AdvantageCard";
-import { animate, motion, Variants } from "framer-motion";
-
-const statsContainerVars: Variants = {
-  animate: {
-    transition: {
-      staggerChildren: 0.07,
-      delayChildren: 0.3,
-    },
-  },
-};
 
 const statusVars: Variants = {
   initial: {
     opacity: 0,
-    scale: 0,
+    scale: 0.3,
   },
   animate: {
     opacity: 1,
@@ -28,7 +19,37 @@ const statusVars: Variants = {
   },
   exit: {
     opacity: 0,
-    scale: 0,
+    scale: 0.3,
+  },
+};
+
+const yAnimation: Variants = {
+  initial: {
+    opacity: 0,
+    y: 60,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+  },
+  exit: {
+    opacity: 0,
+    y: 60,
+  },
+};
+
+const xAnimation: Variants = {
+  initial: {
+    opacity: 0,
+    x: -60,
+  },
+  animate: {
+    opacity: 1,
+    x: 0,
+  },
+  exit: {
+    opacity: 0,
+    x: -60,
   },
 };
 
@@ -40,6 +61,7 @@ const AdvantageCard1 = () => {
       description="Make project monitoring a breeze with iScope, instant real-time changes from the work site to you directly, with full transparency and security."
     >
       <div
+        // variants={statsContainerVars}
         className="bg-background p-4"
         style={{
           borderRadius: "0.75rem 0 0.75rem 0",
@@ -47,18 +69,22 @@ const AdvantageCard1 = () => {
       >
         <div className="flex flex-row items-start justify-between gap-x-4">
           <div className="flex flex-row items-center gap-x-2">
-            <div className="relative w-10 h-10 rounded-full overflow-hidden">
+            <motion.div variants={yAnimation} className="relative w-10 h-10 rounded-full overflow-hidden">
               <Image
                 src="https://plus.unsplash.com/premium_photo-1683740128931-8bdefe553f95?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="Avatar"
                 fill
                 sizes="40px"
               />
-            </div>
+            </motion.div>
 
             <div>
-              <p className="text-xs font-semibold mb-1.5">Amanda Rosch</p>
-              <p className="text-xs text-default-500">Assistant Manager</p>
+              <motion.p variants={xAnimation} className="text-xs font-semibold mb-1.5">
+                Amanda Rosch
+              </motion.p>
+              <motion.p variants={xAnimation} className="text-xs text-default-500">
+                Assistant Manager
+              </motion.p>
             </div>
           </div>
 
@@ -67,27 +93,37 @@ const AdvantageCard1 = () => {
 
         <div className="mt-5 flex flex-col gap-y-4 justify-between h-full">
           <div className="flex flex-row items-center justify-between">
-            <p className="text-xs text-default-500">Invoice id</p>
-            <p className="text-xs">#NV-380</p>
+            <motion.p variants={xAnimation} className="text-xs text-default-500">
+              Invoice id
+            </motion.p>
+            <motion.p variants={xAnimation} className="text-xs">
+              #NV-380
+            </motion.p>
           </div>
 
           <div className="flex flex-row items-center justify-between">
-            <p className="text-xs text-default-500">Total hours</p>
-            <p className="text-xs">180 hours</p>
+            <motion.p variants={xAnimation} className="text-xs text-default-500">
+              Total hours
+            </motion.p>
+            <motion.p variants={xAnimation} className="text-xs">
+              180 hours
+            </motion.p>
           </div>
 
           <div className="flex flex-row items-center justify-between">
-            <p className="text-xs text-default-500">Invoice amount</p>
-            <p className="text-xs">
+            <motion.p variants={xAnimation} className="text-xs text-default-500">
+              Invoice amount
+            </motion.p>
+            <motion.p variants={xAnimation} className="text-xs">
               {(2500).toLocaleString("en-SA", {
                 style: "currency",
                 currency: "SAR",
                 trailingZeroDisplay: "stripIfInteger",
               })}
-            </p>
+            </motion.p>
           </div>
 
-          <motion.div className="flex flex-row gap-x-2 w-full" variants={statsContainerVars}>
+          <div className="flex flex-row gap-x-2 w-full">
             {Array.from({ length: 12 }, (_, i) => (
               <motion.div
                 variants={statusVars}
@@ -98,7 +134,7 @@ const AdvantageCard1 = () => {
                 })}
               ></motion.div>
             ))}
-          </motion.div>
+          </div>
         </div>
       </div>
     </AdvantageCard>
